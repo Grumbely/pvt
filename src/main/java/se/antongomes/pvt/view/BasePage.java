d146c4774c79a9ecdfb1fe866a1219a495a6612c
anton <gomes.anton@gmail.com>
2013-04-03 12:58:50 +0200

    Added a secondary constructor to BasePage, accepting PageParameters.

--- src/main/java/se/antongomes/pvt/view/BasePage.java
+++ src/main/java/se/antongomes/pvt/view/BasePage.java
@@ -1,6 +1,7 @@
 package se.antongomes.pvt.view;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import se.antongomes.pvt.view.panel.LoginPanel;
 import se.antongomes.pvt.view.panel.NavigationPanel;
 
@@ -8,6 +9,11 @@ public class BasePage extends WebPage {
     private static final long serialVersionUID = -5753095673495926422L;
 
     public BasePage() {
+        this(new PageParameters());
+    }
+
+    public BasePage(PageParameters parameters) {
+        super(parameters);
         add(new LoginPanel("loginPanel"));
         add(new NavigationPanel("navPanel"));
     }

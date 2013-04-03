0b48d36814551afa06c938da3e5f2201d4004b98
Anton Gomes <gomes.anton@gmail.com>
2013-04-03 15:28:01 +0200

    Added CSS and JS contribution in BasePage.

--- src/main/java/se/antongomes/pvt/view/BasePage.java
+++ src/main/java/se/antongomes/pvt/view/BasePage.java
@@ -1,11 +1,15 @@
 package se.antongomes.pvt.view;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import se.antongomes.pvt.view.panel.LoginPanel;
 import se.antongomes.pvt.view.panel.NavigationPanel;
 
-public class BasePage extends WebPage {
+public class BasePage extends WebPage implements IHeaderContributor {
     private static final long serialVersionUID = -5753095673495926422L;
 
     public BasePage() {
@@ -16,5 +20,11 @@ public class BasePage extends WebPage {
         super(parameters);
         add(new LoginPanel("loginPanel"));
         add(new NavigationPanel("navPanel"));
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        response.render(CssHeaderItem.forUrl("static/style/pvt.css"));
+        response.render(JavaScriptHeaderItem.forUrl("static/script/pvt.js"));
     }
 }
